ec45a1875fa666140c7982b37262a5c52eaa7627
Caleb Mills <calebmills@mycwi.cc>
2015-03-02 13:35:52 -0700

    Adding files

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Sandbox\webd265\localizationdemo\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Sandbox\webd265\localizationdemo\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes.dex : \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\BuildConfig.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\CordovaApp.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$attr.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$drawable.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$string.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R$xml.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\com\packtpub\phonegaphotshot\localizationdemo\R.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\org\apache\cordova\globalization\Globalization$1.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\org\apache\cordova\globalization\Globalization.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\classes\org\apache\cordova\globalization\GlobalizationError.class \
+C:\Sandbox\webd265\localizationdemo\platforms\android\ant-build\dexedLibs\classes-1ef04808cea431076e916e4a106073ae.jar \
